e0277d43c7b7d6063325c04dc0f9f9b2f31c2e63
kimsuhak <uiouij@naver.com>
2023-12-12 17:40:16 +0900

    item.html 삭제

--- js/item.js
+++ js/item.js
@@ -44,38 +44,50 @@ $.ajax({
 
         var clickItemBox
 
+        var selectedMythicItem = false; // 신화 아이템이 이미 선택됐는지 확인하는 변수
+
         function printItems(filteredallItems) {
             $("#newBox").empty(); // newBox의 초기 값 공백
             for (var i = 0; i < filteredallItems.length; i++) {
                 var item = filteredallItems[i];
                 var imgURL = "http://ddragon.leagueoflegends.com/cdn/13.24.1/img/item/" + item.image.full;
-                var itemButton = $("<button type='button' class='item_box'><img src='" + imgURL + "' alt='" + item.name + "'></button>"+ item.name)
-                var selectedItems = Array(6).fill(null); // 배열로 각 iBox의 상태를 관리
+                var itemButton = $("<button type='button' class='item_box'><img src='" + imgURL + "' alt='" + item.name + "'></button>" + item.name)
 
-                // 아이템 이미지 버튼을 클릭하면, 선택한 아이템 박스에 이미지를 설정하고, 다시 클릭하면 초기화
-                itemButton.click(function () {
-                    var itemName = $(this).find('img').attr('alt');
-                    var imgSrc = $(this).find('img').attr('src');
+                // 아이템 이미지 버튼에 클릭 이벤트를 설정
+                setItemClickEvent(itemButton, item);
 
-                    // 선택하려는 아이템이 이미 선택된 아이템인지 확인
-                    if (selectedItems.includes(itemName)) {
-                        alert("신화템은 중복 될 수 없습니다.");
-                    } else {
-                        if (clickItemBox.find('img').length > 0) {
-                            // clickItemBox.empty(); // 이미지가 이미 있으면, 초기화
-                            clickItemBox.html("<img src='" + imgSrc + "'>"); // 이미지 변경
-                        } else {
-                            clickItemBox.html("<img src='" + imgSrc + "'>"); // 이미지가 없으면 설정
-                            $("#newBox").remove(); // 아이템을 저장하면 #newBox 제거
-                        }
-                        // 클릭한 iBox의 인덱스를 가져와 해당 위치에 아이템을 저장합니다.
-                        var iBoxIndex = parseInt(clickItemBox.attr('id').replace('iBox', ''), 10) - 1;
-                        selectedItems[iBoxIndex] = itemName;
-                    }
-                });
                 $("#newBox").append(itemButton);
             }
         }
+
+                // 아이템 이미지 버튼을 클릭하면, 선택한 아이템 박스에 이미지를 설정하고, 다시 클릭하면 초기화
+                function setItemClickEvent(itemButton, item){
+                itemButton.click(function () {
+                    var imgSrc = $(this).find('img').attr('src');
+
+                        if (clickItemBox.find('img').length > 0) {
+                            clickItemBox.empty(); // 이미지가 이미 있으면, 초기화
+                            selectedMythicItem = false; // 신화 아이템 선택 상태 업데이트
+
+                            $(".cost_ds_flex p").text(": 0원"); // 아이템 가격을 초기화
+                        } else {
+                            // 이미 신화 아이템이 선택된 상태라면 팝업을 띄우고 함수 종료
+                            if (selectedMythicItem) {
+                                alert("신화 아이템은 하나만 선택가능합니다.");
+                                return;
+                            }
+
+                            clickItemBox.html("<img src='" + imgSrc + "'>"); // 이미지가 없으면 설정
+                            $("#newBox").remove(); // 아이템을 저장하면 #newBox 제거
+                            selectedMythicItem = true; // 신화 아이템 선택 상태 업데이트
+
+                            var itemPrice = item.gold.total; // 아이템의 total 값을 추출
+                            $(".cost_ds_flex p").text(": " + itemPrice + " 원"); // 아이템 가격을 HTML에 적용
+                        }
+                });
+
+            }
+
 
         // 6개의 각각의 박스에서 원하는 버튼에 클릭할 경우 기능
             for (var i = 1; i <= 6; i++) {
