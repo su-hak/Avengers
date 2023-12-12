e0c134174f9a2d0995cabe91d7c669a7dcec7c28
kimsuhak <uiouij@naver.com>
2023-12-12 15:17:02 +0900

    item.html 삭제

--- js/item.js
+++ js/item.js
@@ -16,7 +16,7 @@ $.ajax({
                 && items.inStore !== false // 스토어 : false인 item 제외
                 && items.maps["11"] === true; // 소환사의 협곡맵("11")만 출력
         });
-        console.log(filteredallItems.length)
+        // console.log(filteredallItems.length)
 
         // 아이템 알파벳순 정렬
         filteredallItems.sort(function (a, b) {
@@ -53,9 +53,30 @@ $.ajax({
 
                 // 아이템 이미지 버튼을 클릭하면, 선택한 아이템 박스에 이미지를 설정하고, 다시 클릭하면 초기화
                 itemButton.click(function () {
-                    var imgSrc = $(this).find('img').attr('src');
+                    var itemName = $(this).find('img').attr('alt');
+                    var isDuplicate = false;
+
+                    // #iBox1~#iBox6에서 중복된 아이템이 있는지 확인
+                    for (var i = 1; i <= 6; i++) {
+                        if ($("#iBox" + i).find('img').attr('alt') === itemName) {
+                            isDuplicate = true;
+                            break;
+                        }
+                    }
+
+                    // 중복된 아이템이 없을 경우에만 저장
+                    if (!isDuplicate) {
+                        var imgSrc = $(this).find('img').attr('src');
+                        if (clickItemBox.find('img').length > 0) {
+                            clickItemBox.html("<img src='" + imgSrc + "' alt='" + itemName + "'>");  // 이미지 변경
+                        } else {
+                            clickItemBox.html("<img src='" + imgSrc + "' alt='" + itemName + "'>"); // 이미지 설정
+                            $("#newBox").remove(); // 아이템을 저장하면 #newBox 제거
+                        }
+                    }
                     if (clickItemBox.find('img').length > 0) {
-                        clickItemBox.empty(); // 이미지가 이미 있으면, 초기화
+                        // clickItemBox.empty(); // 이미지가 이미 있으면, 초기화
+                        clickItemBox.html("<img src='" + imgSrc + "'>"); // 이미지 변경
                     } else {
                         clickItemBox.html("<img src='" + imgSrc + "'>"); // 이미지가 없으면 설정
                         $("#newBox").remove(); // 아이템을 저장하면 #newBox 제거
@@ -69,15 +90,17 @@ $.ajax({
             for (var i = 1; i <= 6; i++) {
             // 버튼을 클릭하면 아이템 출력
             $("#iBox" + i).click(function () {
-                // 새로운 박스 생성
                 if ($("#newBox").length === 0) {
-                    $("body").append('<div id="newBox"></div>');
+                    $("body").append('<div id="newBox"></div>'); // 새로운 박스 생성
+                } else if (clickItemBox && clickItemBox[0] === this) {
+                    $("#newBox").remove(); // #iBox1~#iBox6을 재클릭하면 #newBox 제거
+                    return; // 이후 코드 실행 방지
                 }
                 clickItemBox = $(this);  // 현재 클릭한 아이템 박스를 저장
                 printItems(filteredallItems); // 아이템 출력을 새로운 박스 안으로 변경
             });
         }
-     }
+    }
 });
 $(document).mouseup(function (e) {
     var container = $("#newBox");
@@ -87,3 +110,5 @@ $(document).mouseup(function (e) {
         container.remove();
     }
 });
+
+
