97b6da594ce255b5012c652b8fc8d46c40b5659e
2klee <148945895+2klee@users.noreply.github.com>
2023-12-20 16:41:57 +0900

    Add files via upload
    공지 모달 정리 필요

--- js/web.js
+++ js/web.js
@@ -14,3 +14,31 @@
 //       triggerButton.style.display = 'block';
 //     });
 //   });
+
+// 위 내용은 재홍이가 test.js 에 첨부
+
+// 아래는 모달창 띄우기
+document.addEventListener('DOMContentLoaded', function() {
+    // Get the modal and button elements
+  var modal = document.getElementById('fullscreenModal');
+  var openModalButton = document.getElementById('openModalButton');
+  var mContent = document.getElementById('modalContent');
+
+  // When the user clicks the button, open the modal
+  openModalButton.onclick = function() {
+      modal.style.display = 'block';
+  };
+
+  // When the user clicks on <span> (x), close the modal
+  function closeModal() {
+    modal.style.display = 'none';
+  }
+
+  // When the user clicks anywhere outside of the modal, close it
+  window.onclick = function(event) {
+      if (event.target == mContent) {
+          modal.style.display = 'none';
+      }
+  };
+});
+
